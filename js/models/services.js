8e0c0080db38343d42e097884a8b139c9facc241
Surya <thotasuryaprakash246@gmail.com>
2016-02-25 12:03:06 +0530

    folder change

--- js/models/services.js
+++ js/models/services.js
@@ -1,47 +1,55 @@
-app.factory('userservice', function ($http) {
-	return {
-		get: function () {
-			return $http.get('js/modules/Users.json');
-        		}
-    	};
-});
-
-app.factory('projectservice', function ($http) {
-	return {
-		get: function () {
-			return $http.get('js/modules/Projects.json');
-        		}
-    	};
-});
-
-app.factory('taskservice', function ($http) {
-	return {
-		get: function () {
-			return $http.get('js/modules/Tasks.json');
-        		}
-    	};
-});
-
 app.factory('allservice', function ($http) {
 	return {
 		getusers: function () {
 			return $http.get('js/modules/Users.json');
-        		},
+        	},
 		getprojects: function () {
 			return $http.get('js/modules/Projects.json');
-        		},
+        	},
 		gettasks: function () {
 			return $http.get('js/modules/Tasks.json');
-        		}
-    	};
+        	}
+    };
 });
 
-app.factory('paramService',function($http,$q){
+
+
+app.factory('userservice', function ($http ,allservice) {
+	return {
+		get: function () {
+			//return allservice.get('js/modules/Users.json');
+			return allservice.getusers();
+        }
+    };
+});
+
+app.factory('projectservice', function ($http,allservice) {
+	return {
+		get: function () {
+			//return $http.get('js/modules/Projects.json');
+			return allservice.getprojects();
+        	
+        }
+    };
+});
+
+app.factory('taskservice', function ($http,allservice) {
+	return {
+		get: function () {
+			//return $http.get('js/modules/Tasks.json');
+			return allservice.gettasks();
+        		
+        }
+    };
+});
+
+
+app.factory('paramService',function($http,$q,allservice){
 	return {
 		get:function(info) {
 			var obj = [];
 			var defered = $q.defer();
-		   	$http.get('js/modules/Projects.json').success(function(data){
+		   	allservice.getprojects().success(function(data){
 	            for(var i=0; i < data.projects.length; i++){
 					for(var j=0; j < data.projects[i].projectMembers.userId.length; j++){
 						if(info  == data.projects[i].projectMembers.userId[j]){
